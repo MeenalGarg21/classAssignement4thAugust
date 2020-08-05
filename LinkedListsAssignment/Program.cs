90f60f49dba66f27295cc0c88d6468f22b421e7d
MeenalGarg <meenal.garg@zensar.com>
2020-08-05 09:09:20 +0530

    Assignment

---
+++ LinkedListsAssignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListsAssignment
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+
+            string[] sentence = {  };
+            LinkedList<string> list = new LinkedList<string>(sentence);
+            foreach(var sent in list)
+            {
+                Console.WriteLine(sent);
+            }
+            Console.WriteLine("adding a node at the top of the element");
+
+
+
+            var Node=list.AddFirst("over");
+            list.AddAfter(Node, "the"); 
+            list.AddFirst("jumps");
+            list.AddFirst("fox");
+            list.AddFirst("brown");
+            list.AddFirst("quick");
+            list.AddFirst("the");
+            var newNode = list.AddLast("fox");
+             
+            list.AddBefore(newNode, "lazy");
+            
+          
+            Console.WriteLine("----------------------finding the element");
+            Console.WriteLine(list.Find("FOX"));
+            
+            foreach (var sent in list)
+            {
+                Console.WriteLine(sent);
+            }
+            Console.ReadLine();
+           
+        }
+    }
+}
